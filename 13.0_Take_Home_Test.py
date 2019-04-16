b88fc68473d6b98d8b3fb6259bfe7d0d4e6b68ad
randallale <randallale@urbandaleschools.com>
2019-04-16 12:09:29 -0500

    AAaaaa

--- 13.0_Take_Home_Test.py
+++ 13.0_Take_Home_Test.py
@@ -1,7 +1,7 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
 but I will definitely not ask another person except the instructor. Signed: Alex Randall
- 
+
 Update the code in this chapter to do the following:
 Open a 500px by 500px window.
 Change the Ball class to a Box class.
@@ -19,27 +19,27 @@ When you're done Pull Request this test and comment that it's ready to be checke
 
 import arcade
 
-SW = 640
-SH = 480
-SPEED = 3    # 3 x 60 = 180 pixels/second
+SW = 500
+SH = 500
+SPEED = 3  # 3 x 60 = 180 pixels/second
 
 
-
-class Ball:
-    def __init__(self, pos_x, pos_y, dx, dy, rad, col):
+class Box:
+    def __init__(self, pos_x, pos_y, dx, dy, size, col):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.dx = dx
         self.dy = dy
-        self.rad = rad
+        self.size = size
         self.col = col
 
     def draw(self):
-        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad, self.col)
+        arcade.draw_rectangle_filled(self.pos_x, self.pos_y, self.size, self.size, self.col)
 
     def update(self):
         self.pos_y += self.dy
         self.pos_x += self.dx
+
 
 class MyGame(arcade.Window):
 
@@ -47,33 +47,54 @@ class MyGame(arcade.Window):
         super().__init__(width, height, title)
         self.set_mouse_visible(False)
         arcade.set_background_color(arcade.color.ASH_GREY)
-        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
-        self.fart1 = arcade.load_sound("Sounds/fart1.mp3")
+        self.blue_box = Box(50, 50, 0, 0, 30, arcade.color.BLUE)
+        self.red_box = Box(50, 50, 0, 0, 30, arcade.color.RED)
+        self.fart2 = arcade.load_sound("Sounds/fart2.mp3")
 
     def on_draw(self):
         arcade.start_render()
-        self.ball.draw()
+        self.red_box.draw()
+        self.blue_box.draw()
 
     def on_update(self, dt):
-        self.ball.update()
+        self.blue_box.update()
+        self.red_box.update()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
-            self.ball.dx = -SPEED
+            self.blue_box.dx = -SPEED
         elif key == arcade.key.RIGHT:
-            self.ball.dx = SPEED
+            self.blue_box.dx = SPEED
         elif key == arcade.key.UP:
-            self.ball.dy = SPEED
+            self.blue_box.dy = SPEED
         elif key == arcade.key.DOWN:
-            self.ball.dy = -SPEED
+            self.blue_box.dy = -SPEED
+
+        if key == arcade.key.A:
+            self.red_box.dx = -SPEED
+        elif key == arcade.key.D:
+            self.red_box.dx = SPEED
+        elif key == arcade.key.W:
+            self.red_box.dy = SPEED
+        elif key == arcade.key.S:
+            self.red_box.dy = -SPEED
+
+        if key == arcade.key.SPACE:
+            arcade.play_sound(self.fart2)
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.dx = 0
-            arcade.play_sound(self.fart1)
+            self.blue_box.dx = 0
 
         elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.dy = 0
+            self.blue_box.dy = 0
+
+        if key == arcade.key.A or key == arcade.key.D:
+            self.red_box.dx = 0
+
+        elif key == arcade.key.W or key == arcade.key.S:
+            self.red_box.dy = 0
+
 
 def main():
     window = MyGame(SW, SH, "Key Press Example")
