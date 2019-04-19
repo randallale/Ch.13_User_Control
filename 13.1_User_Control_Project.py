56ab828b0e6558e1fced087dd8a2d5fd9331c1cb
Alex <randallale@urbandaleschools.com>
2019-04-19 17:23:47 -0500

    Added object detection

--- 13.1_User_Control_Project.py
+++ 13.1_User_Control_Project.py
@@ -21,39 +21,22 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "13.1 User Control Project"
 ROCKET_SPEED = 5
-OBJECT_AMOUNT = 50
+OBJECT_AMOUNT = 10
 
 
-class Objects:
-    def __init__(self, start_x, start_y):
-        self.object_x = start_x
-        self.object_y = start_y
-        self.color = random_color()
-        self.speed = random.randrange(2, 4)
-
-    def draw_object(self):
-        arcade.draw_circle_filled(self.object_x, self.object_y, 8, self.color)
-
-    def update(self):
-        self.object_y -= self.speed
-        if self.object_y <= -10:
-            self.object_y = random.randrange(SCREEN_HEIGHT, SCREEN_HEIGHT+100)
-            self.speed = random.randrange(2, 4)
-
-
-class Rocket:
+class Rocket(object):
     def __init__(self, start_x, start_y):
         self.rocket_x = start_x
         self.rocket_y = start_y
         self.dx = 0
         self.dy = 0
-        self.color = random_color()
+        self.rocket_color = random_color()
 
     def draw_rocket(self):
         tip_list = ((self.rocket_x - 15, self.rocket_y + 30), (self.rocket_x, self.rocket_y + 45),
                     (self.rocket_x + 15, self.rocket_y + 30))
-        arcade.draw_rectangle_filled(self.rocket_x, self.rocket_y, 30, 60, self.color)
-        arcade.draw_polygon_filled(tip_list, self.color)
+        arcade.draw_rectangle_filled(self.rocket_x, self.rocket_y, 30, 60, self.rocket_color)
+        arcade.draw_polygon_filled(tip_list, self.rocket_color)
 
     def reset_pos(self):
         self.rocket_x = SCREEN_WIDTH/2
@@ -66,7 +49,7 @@ class Rocket:
         if self.rocket_y+45 >= SCREEN_HEIGHT:
             self.rocket_y -= 1
             self.dy = 0
-        elif self.rocket_y-45 <= 0:
+        elif self.rocket_y-30 <= 0:
             self.rocket_y += 1
             self.dy = 0
         if self.rocket_x+15 >= SCREEN_WIDTH:
@@ -75,6 +58,65 @@ class Rocket:
         elif self.rocket_x - 15 <= 0:
             self.rocket_x += 1
             self.dx = 0
+
+    def get_location(self, area):
+        if area == "left":
+            rocket_left = self.rocket_x - 15
+            return rocket_left
+        elif area == "right":
+            rocket_right = self.rocket_x + 15
+            return rocket_right
+        elif area == "top":
+            rocket_top = self.rocket_y + 30
+            return rocket_top
+        elif area == "bottom":
+            rocket_bottom = self.rocket_y - 30
+            return rocket_bottom
+
+
+class Objects(Rocket):
+    def __init__(self, object_x, object_y):
+        self.object_x = object_x
+        self.object_y = object_y
+        self.object_color = random_color()
+        self.speed = random.randrange(2, 4)
+
+    def draw_object(self):
+        arcade.draw_circle_filled(self.object_x, self.object_y, 8, self.object_color)
+
+    def update(self):
+        self.object_y -= self.speed
+        print()
+        if self.object_y <= -10:
+            self.object_y = random.randrange(SCREEN_HEIGHT, SCREEN_HEIGHT+100)
+            self.object_x = random.randrange(0, SCREEN_WIDTH)
+            self.object_color = random_color()
+            self.speed = random.randrange(2, 4)
+        if self.object_x == 0:
+            pass
+
+    def get_location(self, area):
+        if area == "left":
+            object_x_left = self.object_x-4
+            return object_x_left
+        elif area == "right":
+            object_x_right = self.object_x+4
+            return object_x_right
+        elif area == "top":
+            object_y_top = self.object_y+4
+            return object_y_top
+        elif area == "bottom":
+            object_y_bottom = self.object_y-4
+            return object_y_bottom
+
+    def stop(self):
+        self.speed = 0
+
+    def reset_pos(self):
+        self.object_y = random.randrange(SCREEN_HEIGHT, SCREEN_HEIGHT + 100)
+        self.object_x = random.randrange(0, SCREEN_WIDTH)
+        self.object_color = random_color()
+        self.speed = random.randrange(2, 4)
 
 
 class MyGame(arcade.Window):
@@ -88,6 +130,7 @@ class MyGame(arcade.Window):
         self.object_list = []
         self.object = None
         self.rocket = None
+        self.score = 0
 
     def setup(self):
         # Create your sprites and sprite lists here
@@ -99,14 +142,14 @@ class MyGame(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("Don't hit the objects!",
+        arcade.draw_text("Only hit your color!",
                          SCREEN_WIDTH / 2, SCREEN_HEIGHT - 28, arcade.color.BLACK, 28, width=2000, align="center",
                          anchor_x="center", anchor_y="center")
 
         # Call draw() on all your sprite lists below
-        self.rocket.draw_rocket()
         for self.object in self.object_list:
             self.object.draw_object()
+        self.rocket.draw_rocket()
 
     def on_update(self, delta_time):
         """
@@ -114,9 +157,22 @@ class MyGame(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
-        self.rocket.update()
         for self.object in self.object_list:
             self.object.update()
+            if self.rocket.get_location("left") < self.object.get_location("left") and \
+                    self.rocket.get_location("right") > self.object.get_location("right") and \
+                    self.rocket.get_location("top") > self.object.get_location("top") and \
+                    self.rocket.get_location("bottom") < self.object.get_location("bottom"):
+                if self.rocket.rocket_color == self.object.object_color:
+                    self.score += 1
+                    self.object.reset_pos()
+                    self.rocket.rocket_color = random_color()
+                elif self.rocket.rocket_color != self.object.object_color:
+                    self.object.reset_pos()
+                    self.rocket.rocket_color = arcade.color.BLACK
+                    self.object.stop()
+                    arcade.draw_text(("Your Score was", self.score), SCREEN_WIDTH/2,20,arcade.color.BLACK,20,width=2000,align="center",anchor_x="center, anchor_y="center"
+        self.rocket.update()
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
@@ -150,7 +206,6 @@ def random_color():
 
 
 def main():
-    """ Main method """
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
     arcade.run()
--- Testing.py
+++ Testing.py
@@ -1,63 +1,21 @@
-import arcade
+class ClassA(object):
+    var1 = 0
+    var2 = 0
+    def __init__(self):
+        ClassA.var1 = 1
+        ClassA.var2 = 2
 
-SW = 640
-SH = 480
-SPEED = 3    # 3 x 60 = 180 pixels/second
+    def methodA(self):
+        ClassA.var1 = ClassA.var1 + ClassA.var2
+        return ClassA.var1
 
 
+class ClassB(ClassA):
+    def __init__(self):
+        print(ClassA.var1)
+        print(ClassA.var2)
 
-class Ball:
-    def __init__(self, pos_x, pos_y, dx, dy, rad, col):
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-        self.dx = dx
-        self.dy = dy
-        self.rad = rad
-        self.col = col
-
-    def draw(self):
-        arcade.draw_circle_filled(self.pos_x, self.pos_y, self.rad, self.col)
-
-    def update(self):
-        self.pos_y += self.dy
-        self.pos_x += self.dx
-
-class MyGame(arcade.Window):
-
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
-        self.set_mouse_visible(False)
-        arcade.set_background_color(arcade.color.ASH_GREY)
-        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
-        self.fart1 = arcade.load_sound("Sounds/fart1.mp3")
-
-    def on_draw(self):
-        arcade.start_render()
-        self.ball.draw()
-
-    def on_update(self, dt):
-        self.ball.update()
-
-    def on_key_press(self, key, modifiers):
-        if key == arcade.key.LEFT:
-            self.ball.dx = -SPEED
-        elif key == arcade.key.RIGHT:
-            self.ball.dx = SPEED
-        elif key == arcade.key.UP:
-            self.ball.dy = SPEED
-        elif key == arcade.key.DOWN:
-            self.ball.dy = -SPEED
-
-    def on_key_release(self, key, modifiers):
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.dx = 0
-            arcade.play_sound(self.fart1)
-
-        elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.dy = 0
-
-def main():
-    window = MyGame(SW, SH, "Key Press Example")
-    arcade.run()
-
-main()
+object1 = ClassA()
+sum = object1.methodA()
+object2 = ClassB()
+print(sum)
